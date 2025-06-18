3978f680f00b34fd54ef9d080e5cd057fe0ea64f
FluffyChi-Xing <121754413+FluffyChi-Xing@users.noreply.github.com>
2025-06-18 20:55:14 +0800

    更改配置

--- ddd_mall/application/src/main/java/com/mall/application/Application.java
+++ ddd_mall/application/src/main/java/com/mall/application/Application.java
@@ -1,9 +1,13 @@
 package com.mall.application;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.mall.application", "com.mall.infrastruction.dao"})
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
